1d461fb04c85585e95d8ec58e9a8cfc24ff5306c
richardMRa <richete05@gmail.com>
2023-01-29 18:00:39 -0500

    fix client project dev env

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-
+import reactLogo from './assets/react.svg'
 import './styles/App.css'
 
-const App = () => {
-  
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
-        
+    
     </div>
   )
 }
